cddf2c9d04a7552ee21c3b78f3d0c6d033d45be7
rauelon <rauel.anders@web.de>
2026-05-10 13:09:23 +0200

    ADDED: Remote connection

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
 from flask import Flask
-from models import db, Movie
 from data_manager import DataManager
+from models import db, Movie
+
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
 data_manager = DataManager()
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return "Hello Flask"
-
-
-@app.route("/add_movie")
-def add_movie():
-
-    new_movie = Movie(
-        name="Interstellar",
-        director="Christopher Nolan",
-        year=2014,
-        poster_url="https://...",
-        user_id=1
-    )
-
-    data_manager.add_movie(new_movie)
-
-    return "Movie added successfully!"
+    return "Welcome to MoviWeb App!"
 
 
 def main():
