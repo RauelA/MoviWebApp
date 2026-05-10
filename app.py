84a4c610c887b08e2b5ebee6d8c7f261cb388211
rauelon <rauel.anders@web.de>
2026-05-10 14:29:35 +0200

    ADDED: Error handling, try/except for 'data_manager.py'

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
+import os
 import requests
 from flask import Flask, request, redirect, url_for, render_template
 from data_manager import DataManager
 from models import db, Movie
-import os
-
-
-OMDB_API_KEY = os.getenv("c0772217")
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.join(basedir, "data")
+os.makedirs(data_dir, exist_ok=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,6 +17,18 @@ db.init_app(app)
 
 data_manager = DataManager()
 
+OMDB_API_KEY = os.getenv("c0772217")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
 def index():
@@ -26,23 +36,25 @@ def index():
     return render_template('index.html', users=users)
 
 
-@app.route('/users')
-def list_users():
-    users = data_manager.get_users()
-    return str(users)
-
-
 @app.route('/users', methods=['POST'])
 def add_user():
+
     name = request.form.get('name')
+
+    if not name:
+        return "Missing user name", 400
+
     data_manager.create_user(name)
-    return redirect(url_for('home'))
+
+    return redirect(url_for('index'))
 
 
-@app.route('/users/<int:user_id>/movies')
+@app.route('/users/<int:user_id>/movies', methods=['GET'])
 def user_movies(user_id):
+
     movies = data_manager.get_movies(user_id)
-    return str(movies)
+
+    return render_template('movies.html', movies=movies, user_id=user_id)
 
 
 @app.route('/users/<int:user_id>/movies', methods=['POST'])
@@ -50,15 +62,30 @@ def add_movie(user_id):
 
     title = request.form.get('title')
 
-    response = requests.get(f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}")
+    if not title:
+        return "Missing movie title", 400
 
+    # OMDb call
+    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
+    response = requests.get(url)
     data = response.json()
 
+    # SAFE FALLBACKS
+    movie_name = data.get("Title") or title
+    director = data.get("Director") or "Unknown"
+    poster = data.get("Poster") or ""
+    year_raw = data.get("Year")
+
+    try:
+        year = int(year_raw[:4]) if year_raw and year_raw != "N/A" else 0
+    except:
+        year = 0
+
     new_movie = Movie(
-        name=data.get("Title"),
-        director=data.get("Director"),
-        year=int(data.get("Year", 0)[:4]) if data.get("Year") else 0,
-        poster_url=data.get("Poster"),
+        name=movie_name,
+        director=director,
+        year=year,
+        poster_url=poster,
         user_id=user_id
     )
 
@@ -69,7 +96,11 @@ def add_movie(user_id):
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
 def update_movie(user_id, movie_id):
+
     new_title = request.form.get('title')
+
+    if not new_title:
+        return "Missing title", 400
 
     data_manager.update_movie(movie_id, new_title)
 
@@ -78,6 +109,7 @@ def update_movie(user_id, movie_id):
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
 def delete_movie(user_id, movie_id):
+
     data_manager.delete_movie(movie_id)
 
     return redirect(url_for('user_movies', user_id=user_id))
--- data_manager.py
+++ data_manager.py
@@ -1,38 +1,73 @@
 from models import db, User, Movie
 
+
 class DataManager:
 
     def create_user(self, name):
-        new_user = User(name=name)
-        db.session.add(new_user)
-        db.session.commit()
+        try:
+            user = User(name=name)
+            db.session.add(user)
+            db.session.commit()
+            return user
+
+        except Exception as e:
+            db.session.rollback()
+            print("Error in create_user:", str(e))
+            return None
 
     def get_users(self):
-        return User.query.all()
+        try:
+            return User.query.all()
+
+        except Exception as e:
+            print("Error in get_users:", str(e))
+            return []
 
     def get_movies(self, user_id):
-        return Movie.query.filter_by(user_id=user_id).all()
+        try:
+            return Movie.query.filter_by(user_id=user_id).all()
+
+        except Exception as e:
+            print("Error in get_movies:", str(e))
+            return []
 
     def add_movie(self, movie):
-        db.session.add(movie)
-        db.session.commit()
+        try:
+            db.session.add(movie)
+            db.session.commit()
+            return movie
 
-        return movie
+        except Exception as e:
+            db.session.rollback()
+            print("Error in add_movie:", str(e))
+            return None
 
     def update_movie(self, movie_id, new_title):
-        movie = Movie.query.get(movie_id)
+        try:
+            movie = Movie.query.get(movie_id)
 
-        if movie:
-            movie.name = new_title
-            db.session.commit()
+            if movie:
+                movie.name = new_title
+                db.session.commit()
 
-        return movie
+            return movie
+
+        except Exception as e:
+            db.session.rollback()
+            print("Error in update_movie:", str(e))
+            return None
 
     def delete_movie(self, movie_id):
-        movie = Movie.query.get(movie_id)
+        try:
+            movie = Movie.query.get(movie_id)
 
-        if movie:
-            db.session.delete(movie)
-            db.session.commit()
+            if movie:
+                db.session.delete(movie)
+                db.session.commit()
 
-        return movie
+            return movie
+
+        except Exception as e:
+            db.session.rollback()
+            print("Error in delete_movie:", str(e))
+            return None
